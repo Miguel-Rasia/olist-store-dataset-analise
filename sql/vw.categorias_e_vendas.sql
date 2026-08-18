0582ef3449648576e3ab635b7637f0b202fa20ab
Miguel Rasia <gomesrasialuizmiguel@gmail.com>
2026-08-18 19:46:45 -0300

    Adicionado o projeto de análise de vendas da Olist Store

---
+++ sql/vw.categorias_e_vendas.sql
@@ -0,0 +1,82 @@
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('beleza_saude',1258662.1,8836),
+	 ('relogios_presentes',1204992.2,5624),
+	 ('cama_mesa_banho',1036944.6,9417),
+	 ('esporte_lazer',988029.44,7720),
+	 ('informatica_acessorios',911938.44,6689),
+	 ('moveis_decoracao',729753.5,6449),
+	 ('cool_stuff',635296.6,3632),
+	 ('utilidades_domesticas',632253.4,5884),
+	 ('automotivo',592726.4,3897),
+	 ('ferramentas_jardim',485267.3,3518);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('brinquedos',483956.8,3886),
+	 ('bebes',411770.94,2885),
+	 ('perfumaria',399132.1,3162),
+	 ('telefonia',323669.72,4199),
+	 ('moveis_escritorio',273959.4,1273),
+	 ('papelaria',230944.66,2311),
+	 ('pcs',222963.08,181),
+	 ('pet_shop',214315.88,1710),
+	 ('instrumentos_musicais',191499.12,628),
+	 ('eletroportateis',190648.67,630);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('',179535.05,1451),
+	 ('eletronicos',160247.22,2550),
+	 ('consoles_games',157465.23,1062),
+	 ('fashion_bolsas_e_acessorios',152823.31,1864),
+	 ('construcao_ferramentas_construcao',144677.45,748),
+	 ('malas_acessorios',140429.55,1034),
+	 ('eletrodomesticos_2',113317.7,234),
+	 ('casa_construcao',83088.016,490),
+	 ('eletrodomesticos',80171.39,764),
+	 ('agro_industria_e_comercio',72530.43,182);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('moveis_sala',68916.33,422),
+	 ('telefonia_fixa',59582.875,217),
+	 ('casa_conforto',58571.875,397),
+	 ('climatizacao',55024.895,253),
+	 ('audio',50688.438,350),
+	 ('portateis_casa_forno_e_cafe',47445.703,75),
+	 ('livros_interesse_geral',46856.777,512),
+	 ('moveis_cozinha_area_de_servico_jantar_e_jardim',46328.324,248),
+	 ('construcao_ferramentas_iluminacao',41079.99,244),
+	 ('construcao_ferramentas_seguranca',40544.523,167);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('industria_comercio_e_negocios',39669.605,235),
+	 ('alimentos',29393.5,450),
+	 ('market_place',28378.518,280),
+	 ('construcao_ferramentas_jardim',25715.9,194),
+	 ('artes',24202.658,202),
+	 ('fashion_calcados',23562.826,240),
+	 ('bebidas',22428.738,297),
+	 ('sinalizacao_e_seguranca',21509.238,140),
+	 ('moveis_quarto',20028.785,95),
+	 ('livros_tecnicos',19096.072,260);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('construcao_ferramentas_ferramentas',15903.952,97),
+	 ('alimentos_bebidas',15179.489,227),
+	 ('fashion_roupa_masculina',10797.825,112),
+	 ('fashion_underwear_e_moda_praia',9541.55,121),
+	 ('artigos_de_natal',8800.82,128),
+	 ('tablets_impressao_imagem',7528.4097,79),
+	 ('cine_foto',6933.458,65),
+	 ('musica',6034.3496,38),
+	 ('dvds_blu_ray',5999.3877,59),
+	 ('livros_importados',4639.85,53);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('artigos_de_festas',4485.1787,39),
+	 ('moveis_colchao_e_estofado',4368.0796,38),
+	 ('portateis_cozinha_e_preparadores_de_alimentos',3968.5303,14),
+	 ('fashion_roupa_feminina',2803.6394,39),
+	 ('fashion_esporte',2119.5103,27),
+	 ('la_cuisine',2054.99,13),
+	 ('artes_e_artesanato',1814.0103,23),
+	 ('fraldas_higiene',1567.59,27),
+	 ('pc_gamer',1545.95,8),
+	 ('flores',1110.04,29);
+INSERT INTO public.vw_categories_and_sales (product_category_name,total_revenue,total_orders) VALUES
+	 ('casa_conforto_2',760.2702,24),
+	 ('cds_dvds_musicais',730.0,12),
+	 ('fashion_roupa_infanto_juvenil',569.85,8),
+	 ('seguros_e_servicos',283.28998,2);
